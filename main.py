e0d2dae0630cf6634ba31b9e9c71ca530af46e97
JulianCeccacci <90716414+julianceccacci2005@users.noreply.github.com>
2021-09-29 15:51:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Description: 
+
+# Read in the earnings for each month
+print("Enter your earnings for each month.")
+may_earnings = input("May: $")
+jun_earnings = input("June: $")
+jul_earnings = input("July: $")
+aug_earnings = input("August: $")
+print()
